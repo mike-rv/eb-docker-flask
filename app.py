32ef25f548d541134b1b4323e3f1bb4a26f53c20
Mati <mati@Matis-MacBook-Air.local>
2024-03-15 05:09:56 +0000

    app works

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Mati!"
+    return "Hello, world!"
 
 # This condition ensures that the app only runs if this script is executed directly
 if __name__ == '__main__':
